9abd39210307b6f28b26f3a8d4f1015ff25b83ca
Adam O'Neil <adamosoftware@gmail.com>
2025-08-15 18:20:33 -0400

    test case

---
+++ Testing/Resources/Case01/Status.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Testing.Conventions;
+
+namespace Testing.Resources.Case01;
+
+public class Status : BaseTable
+{
+	public string Name { get; set; } = default!;
+	public string? Description { get; set; }
+
+	public ICollection<Order> Orders { get; set; } = [];
+}
+
+public class StatusConfiguration : IEntityTypeConfiguration<Status>
+{
+	public void Configure(EntityTypeBuilder<Status> builder)
+	{		
+		builder.Property(s => s.Name).IsRequired().HasMaxLength(50);
+		builder.Property(s => s.Description).HasMaxLength(255);		
+	}
+}

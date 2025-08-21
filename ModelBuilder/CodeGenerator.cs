1e4de30471fe620757d11389facad001d6d53ad2
Adam O'Neil <adamosoftware@gmail.com>
2025-08-20 20:43:34 -0400

    let's go with this

--- ModelBuilder/CodeGenerator.cs
+++ ModelBuilder/CodeGenerator.cs
@@ -4,7 +4,20 @@ namespace ModelBuilder;
 
 public class CodeGenerator
 {
-	public static string Execute(LocalSettings settings, EntityDefinition entity)
+	public class Settings
+	{
+		public string IdentityType { get; set; } = "int";
+		/// <summary>
+		/// for example SpayWise.Data
+		/// </summary>
+		public string DefaultNamespace { get; set; } = "Generated";
+		/// <summary>
+		/// for example SpayWise.Data.Conventions
+		/// </summary>
+		public string? BaseClassNamespace { get; set; }
+	}
+
+	public static string Execute(Settings settings, EntityDefinition entity, EntityDefinition[] allEntities)
 	{
 		StringBuilder sb = new();
 		sb.AppendLine(
@@ -22,27 +35,89 @@ public class CodeGenerator
 			namespace {settings.DefaultNamespace};
 			""");
 
-		sb.AppendLine($"public class {entity.Name}\r\n{{");
+		sb.AppendLine(ClassDeclaration(entity));
 
 		AddProperties(sb, entity);
-		AddNavigationProperties(sb, entity);
+		sb.AppendLine();
+		AddParentNavProperties(sb, entity);
+		AddChildCollections(sb, entity.Name, allEntities);
 
 		sb.AppendLine("}");
 
+		sb.AppendLine();
+
+		sb.AppendLine($"public class {entity.Name}Configuration : IEntityTypeConfiguration<{entity.Name}>\r\n{{");
+		sb.AppendLine($"\tpublic void Configure(EntityTypeBuilder<{entity.Name}> builder)\r\n\t{{");
+		AddConfiguration(sb, entity);
+		sb.AppendLine("\t}");
+		sb.AppendLine("}");
+
 		return sb.ToString();
+
+		static string ClassDeclaration(EntityDefinition e) => 
+			$"public class {e.Name}" +
+				(!string.IsNullOrWhiteSpace(e.BaseClass) ? $" : {e.BaseClass}" : string.Empty) + 
+				"\r\n{";
 	}
 
-	private static void AddNavigationProperties(StringBuilder sb, EntityDefinition entity)
+	private static void AddConfiguration(StringBuilder sb, EntityDefinition entity)
 	{
-		throw new NotImplementedException();
+		var uniqueProperties = entity.Properties.Where(p => p.IsUnique).ToArray();
+		if (uniqueProperties.Any())
+		{
+			sb.AppendLine($"\t\tbuilder.HasIndex(e => new {{ {string.Join(", ", uniqueProperties.Select(p => $"e.{p.Name}"))} }}).IsUnique());");
+		}
+
+		foreach (var prop in entity.Properties.Where(p => p.MaxLength.HasValue))
+		{
+			sb.AppendLine($"\t\tbuilder.Property(e => e.{prop.Name}).HasMaxLength({prop.MaxLength!.Value});");
+		}
+
+		foreach (var prop in entity.Properties.Where(p => !string.IsNullOrWhiteSpace(p.ReferencedEntity)))
+		{
+			sb.AppendLine($"\t\tbuilder.HasOne(e => e.{prop.ReferencedEntity}).WithMany(e => e.{prop.ChildCollection}).HasForeignKey(p => {prop.Name}).OnDelete();");
+		}
+	}
+
+	private static void AddChildCollections(StringBuilder sb, string entityName, IEnumerable<EntityDefinition> allEntities)
+	{
+		var childCollections = allEntities
+			.SelectMany(
+				e => e.Properties.Where(p => p.ReferencedEntity?.Equals(entityName) ?? false && p.ChildCollection is not null),
+				(e, p) => (e.Name, p.ChildCollection));
+			
+		foreach (var (childEntity, childCollection) in childCollections)
+		{
+			sb.AppendLine($"\tpublic ICollection<{childEntity}> {childCollection} {{ get; set; }} = [];");
+		}
+	}
+
+	private static void AddParentNavProperties(StringBuilder sb, EntityDefinition entity)
+	{
+		foreach (var prop in entity.Properties.Where(p => !string.IsNullOrWhiteSpace(p.ReferencedEntity)))
+		{
+			sb.AppendLine($"\tpublic {prop.ReferencedEntity}? {TrimIdEnding(prop.Name)} {{ get; set; }}");
+		}
+
+		static string TrimIdEnding(string name) =>
+			name.EndsWith("Id", StringComparison.OrdinalIgnoreCase) ? name[0..^2] : name;
 	}
 
 	private static void AddProperties(StringBuilder sb, EntityDefinition entity)
 	{
-		throw new NotImplementedException();
+		foreach (var prop in entity.Properties)
+		{
+			sb.AppendLine($"\tpublic {prop.ClrType}{NullableExpression(prop)} {prop.Name} {{ get; set; }}{DefaultExpression(prop)}");
+		}
+
+		static string NullableExpression(PropertyDefinition prop) =>
+			prop.IsNullable ? "?" : string.Empty;
+
+		static string DefaultExpression(PropertyDefinition prop) =>
+			!string.IsNullOrWhiteSpace(prop.DefaultValue) ? $" = {prop.DefaultValue}" : string.Empty;
 	}
 
-	public static void WriteFiles(LocalSettings settings, EntityDefinition[] entities, string outputFolder)
+	public static void WriteFiles(Settings settings, EntityDefinition[] entities, string outputFolder)
 	{
 		foreach (var entity in entities)
 		{
@@ -55,11 +130,11 @@ public class CodeGenerator
 				continue;
 			}
 			
-			var code = Execute(settings, entity);
+			var code = Execute(settings, entity, entities);
 			File.WriteAllText(outputFile, code);
 		}
 	}
 
-	private static string UsingBaseClassNamespace(LocalSettings settings) =>
+	private static string UsingBaseClassNamespace(Settings settings) =>
 		string.IsNullOrWhiteSpace(settings.BaseClassNamespace) ? string.Empty : $"using {settings.BaseClassNamespace};";
 }

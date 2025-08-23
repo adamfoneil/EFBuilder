686378e89a0b686f7411e070f77de03683cf792a
Adam O'Neil <adamosoftware@gmail.com>
2025-08-23 18:18:57 -0400

    removed obsolete project

--- EFBuilder/Models/EFBuilderSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace EFBuilder.Models;
-
-public class EFBuilderSettings
-{
-    public string DefaultNamespace { get; set; } = "Generated";
-    public string BaseClassNamespace { get; set; } = "Testing.Conventions";
-}
